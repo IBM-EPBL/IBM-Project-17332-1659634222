5b867e9343e3820470092171d6e86f14f8880dd3
niranjanrajkumar <59790355+niranjanrajkumar@users.noreply.github.com>
2022-11-16 20:04:36 +0530

    Update db.py

--- Implementing Web Application/Create IBM DB2 and connect with python/db.py
+++ Implementing Web Application/Create IBM DB2 and connect with python/db.py
@@ -1,4 +1,4 @@
 import ibm_db
-ibm_db.pconnect("DATABASE=bludb;HOSTNAME=2f3279a5-73d1-4859-88f0-a6c3e6b4b907.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30756;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=psg63208;PWD=rcwq7Y11nzdZGs6T;",'','')
+ibm_db.pconnect("DATABASE=bludb;HOSTNAME=2f3279a5-73d1-4859-88f0-a6c3e6b4b907.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30756;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=;PWD=;",'','')
 print("connection successful...")
 
